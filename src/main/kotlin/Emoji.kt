8840e4f5d2cbbbc46b26d08d9c779539004bf989
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2023-03-11 00:04:53 +0100

    Move to jsdelivr links pointing at twemoji@master

--- src/main/kotlin/Emoji.kt
+++ src/main/kotlin/Emoji.kt
@@ -16,7 +16,7 @@ data class Emoji(val codes: String, val char: String, val name: String) {
             .lowercase()
             .replace(" ", "-")
 
-    private val twemojiUrl: String = "https://twemoji.maxcdn.com/v/latest/svg/$normalizedCodes.svg"
+    private val twemojiUrl: String = "https://cdn.jsdelivr.net/gh/twitter/twemoji@master/assets/svg/$normalizedCodes.svg"
 
     val cssClass: String = """
             .twa-${normalizedName} {
